3005fadc5a9e4d018a01dea2317b535005162401
Sean Demske <seandemske@gmail.com>
2021-01-11 13:48:13 -0500

    added user registration

--- routes/rootRoutes.js
+++ routes/rootRoutes.js
@@ -1,5 +1,7 @@
 // Routes that begin at the root "/"
 const Router = require("express").Router;
+const User = require("../models/User");
+const ExpressError = require("../expressError");
 const db = require("../db");
 
 const router = new Router();
@@ -15,15 +17,10 @@ router.get("/signup", function(req, res, next) {
 
 router.post("/signup", async function(req, res, next) {
     const { username, password, confirmPassword, email } = req.body;
-    try {
-        const result = await db.query(
-            `INSERT INTO users (username, email, password)
-            VALUES ($1, $2, $3)
-            RETURNING username`,
-            [username, email, password]
-        );
-        const user = result.rows[0];
 
+    User.checkRegisterPasswordsMatch(password, confirmPassword, next);
+    try {
+        let user = await User.register({ username, password, email });
         return res.redirect(`/${user.username}`);
     } catch(err) {
         return next(err);
@@ -32,6 +29,17 @@ router.post("/signup", async function(req, res, next) {
 
 router.get("/signin", function(req, res, next) {
     return res.render("signin_form.html");
+});
+
+router.post("/signin", async function(req, res, next) {
+    const { username, password } = req.body;
+
+    const user = await User.authenticate(username, password);
+    if (user === true) {
+        return res.redirect(`/${user.username}`);
+    } else {
+        return res.redirect("/signin");
+    }
 });
 
 module.exports = router;
